f20d4a16aad8ace67d2ed975c8b1fc1b6c53ee8d
José Cañizares <jose.canizares@colorado.edu>
2017-01-16 00:34:59 -0700

    Add import statements for each component

--- src/components/header_arrow/HeaderArrow.js
+++ src/components/header_arrow/HeaderArrow.js
@@ -11,7 +11,7 @@ var HeaderArrow = React.createClass({
                 <defs>
                   <mask id="mask" x="0" y="0" width="100" height="50">
                     <rect x="0" y="0" width="100" height="40" fill="#fff"/>
-                    <text textAnchor="middle" x="50" y="27" dy="1" fontFamily="Futura" fontSize="19">V</text>
+                    <text textAnchor="middle" x="50" y="32" dy="1" fontFamily="Futura" fontSize="49">&#8964;</text>
                   </mask>
                 </defs>
                 <circle fill="white" cx="50" cy="20" r="20" mask="url(#mask)" fillOpacity="0.9"/>
