f0c9b93ae3f6ff9a92ea451650f294bd34a2a8d5
José Cañizares <jose.canizares@colorado.edu>
2017-05-15 13:26:37 -0600

    basic home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
 import Home from './Home.js';
-import About from './About.js';
-import Contact from './Contact.js';
+import About from './pages/About.js';
+import Contact from './pages/Contact.js';
 
 
 class App extends Component {
---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Section from '../components/Section.js';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="contact">
+        This is our contact:
+      </div>
+    );
+  }
+}
+
+export default Contact;
