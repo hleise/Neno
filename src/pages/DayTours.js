fdc77d365e8521b149b5a2051e6856025cfd8f16
Igor Banaszuk <igorbanaszuk@Igors-MacBook-Pro.local>
2017-05-16 16:38:06 -0600

    4 sections of Day Tours

--- src/pages/DayTours.js
+++ src/pages/DayTours.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import Section from '../components/Section.js';
+import NavBar from '../components/NavBar.js';
 
 class DayTours extends Component {
   render() {
     return (
-      <div>
-        Day Tours
+      <div className="day-tour-page">
+      <NavBar/>
+      <div className="culture-day-tours">
+            <p id="culture-text">
+              Cultural Day Tours
+            </p>
+            <img className='culture-image' src={require('../img/0.jpg')} />
+            <div className="culture-horizontal">
+                
+            </div>
+        </div>
+        <div className="boat-tours">
+          <p id="boat-text">
+            Boat Tours
+          </p>
+          <img className='boat-image' src={require('../img/3.jpg')} />
+        </div>
+        <div className="fishing-tours">
+          <p id="fish-text">
+           Fishing Tours
+          </p>
+           <img className='boat-image' src={require('../img/deep_fishing.jpg')} />
+        </div>
+        <div className="other-tours">
+        <p id="other-text">
+         Other Tours
+        </p>
+         <img className='boat-image' src={require('../img/1.jpg')} />
+        </div>
       </div>
     );
   }
