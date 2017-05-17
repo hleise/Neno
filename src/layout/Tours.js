1fec7669ced7598dd57d328d724507fc01460610
José Cañizares <jose.canizares@colorado.edu>
2017-05-17 16:25:42 -0600

    Create tours layout

---
+++ src/layout/Tours.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import DayToursData from '../data/DayTours.json';
+
+class Tours extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menus: [
+        "closed",
+        "closed",
+        "closed",
+        "closed"
+      ]
+    }
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+  toggleMenu(i) {
+    this.setState((prevState, props) => {
+      var newMenus = prevState.menus;
+      if(prevState.menus[i] == "closed") {
+        newMenus[i] = "open";
+      } else {
+        newMenus[i] = "closed";
+      }
+      return {
+          menus: newMenus
+      }
+    });
+  }
+  render() {
+    return (
+      <div className="tours">
+        {DayToursData.map((section, i) =>
+        <div className='tours-section'>
+          <div className="tours-banner">
+            <h1 onMouseDown={this.toggleMenu.bind(this, i)} className="section-title">
+              {section.sectionTitle + " Tours"}
+            </h1>
+           <img className='section-image' src={require("../img/" + this.props.page + "/" + i + ".jpg")} />
+          </div>
+          <div className={"tours-menu" + " " + this.state.menus[i]}>
+            <div className='featured-tours'>
+              <div className='tours-row'>
+                {section.featuredTours.slice(0,4).map((tourImg, i) =>
+                  <div className='tour-block'>
+                    <img className='tour-image' src={require("../img/" + this.props.page + "/"  + section.imgFolder + "/" + i + ".jpg")} />
+                  </div>
+                )}
+              </div>
+              <div className='tours-row'>
+                {section.featuredTours.slice(4,8).map((tourImg, i) =>
+                  <div className='tour-block'>
+                    <img className='tour-image' src={require("../img/" + this.props.page + "/"  + section.imgFolder + "/" + (i+4) + ".jpg")} />
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Tours;
--- src/pages/DayTours.js
+++ src/pages/DayTours.js
@@ -1,66 +1,12 @@
 import React, { Component } from 'react';
 import DayToursData from '../data/DayTours.json';
+import ToursLayout from '../layout/Tours';
 
 class DayTours extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menus: [
-        "closed",
-        "closed",
-        "closed",
-        "closed"
-      ]
-    }
-    this.toggleMenu = this.toggleMenu.bind(this);
-  }
-  toggleMenu(i) {
-    this.setState((prevState, props) => {
-      var newMenus = prevState.menus;
-      if(prevState.menus[i] == "closed") {
-        newMenus[i] = "open";
-      } else {
-        newMenus[i] = "closed";
-      }
-      return {
-          menus: newMenus
-      }
-    });
-  }
   render() {
-    // const row1 = section.featuredTours.slice(0,5);
-    // const row2 = section.featuredTours.slice(5,9);
-    // const row3 = section.featuredTours.slice(9,13);
     return (
       <div className="day-tours">
-        {DayToursData.map((section, i) =>
-        <div className='tours-section'>
-          <div className="tours-banner">
-            <h1 onMouseDown={this.toggleMenu.bind(this, i)} className="section-title">
-              {section.sectionTitle + " Tours"}
-            </h1>
-           <img className='section-image' src={require("../img/day-tours/" + i + ".jpg")} />
-          </div>
-          <div className={"tours-menu" + " " + this.state.menus[i]}>
-            <div className='featured-tours'>
-              <div className='tours-row'>
-                {section.featuredTours.slice(0,4).map((tourImg, i) =>
-                  <div className='tour-block'>
-                    <img className='tour-image' src={require("../img/day-tours/"  + section.imgFolder + "/" + i + ".jpg")} />
-                  </div>
-                )}
-              </div>
-              <div className='tours-row'>
-                {section.featuredTours.slice(4,8).map((tourImg, i) =>
-                  <div className='tour-block'>
-                    <img className='tour-image' src={require("../img/day-tours/"  + section.imgFolder + "/" + (i+4) + ".jpg")} />
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-        )}
+        <ToursLayout page="day-tours"/>
       </div>
     );
   }
