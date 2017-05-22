cb6ffaba378233aaaeaa6b0aa1b1d39588a28818
Igor Banaszuk <igorbanaszuk@Igors-MacBook-Pro.local>
2017-05-22 15:32:17 -0600

    all 5 markdowns for car rental exist but need to reimplement with jose

--- src/pages/CarRental.js
+++ src/pages/CarRental.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+/* text of page styled and wrapped like home */
 import TextSection from '../components/TextSection.js';
+/*import 5 .md for 5 vehicles*/
 import markdown1 from '../content/CarRental/Motorcyle.md';
 import markdown2 from '../content/CarRental/Vespa.md';
 import markdown3 from '../content/CarRental/VitaraPackage1.md';
@@ -7,15 +9,26 @@ import markdown4 from '../content/CarRental/VitaraPackage2.md';
 import markdown5 from '../content/CarRental/VitaraPackage3.md';
 
 class Section extends Component {
-  rawMarkup(){
-           return{ __html: markdown1,markdown2,markdown3,markdown4,markdown5};
-  }
+  /*let us reimplement as one function that returns an array of .md to assign to specifc div */
+  createMarkup1(){return{ __html: markdown1};}
+  createMarkup2(){return{ __html: markdown2};}
+  createMarkup3(){return{ __html: markdown3};}
+  createMarkup4(){return{ __html: markdown4};}
+  createMarkup5(){return{ __html: markdown5};}
   render() {
     return (
       <div className='car-rental'>
       <TextSection>
         <h1>Car Rental</h1>
-          <div className='home-markdown' dangerouslySetInnerHTML={this.rawMarkup()}>
+          <div className='car-markdown1' dangerouslySetInnerHTML={this.createMarkup1()}>
+          </div>
+          <div className='car-markdown2' dangerouslySetInnerHTML={this.createMarkup2()}>
+          </div>
+          <div className='car-markdown3' dangerouslySetInnerHTML={this.createMarkup3()}>
+          </div>
+          <div className='car-markdown4' dangerouslySetInnerHTML={this.createMarkup4()}>
+          </div>
+          <div className='car-markdown5' dangerouslySetInnerHTML={this.createMarkup5()}>
           </div>
       </TextSection>
       </div>
