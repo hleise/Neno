3300e19186ddf04b8e800fd45da8c6bb1eb34ecc
Igor Banaszuk <igorbanaszuk@Igors-MacBook-Pro.local>
2017-05-23 11:55:37 -0600

    fixed the car rental page with jose

--- src/pages/FlightInfo.js
+++ src/pages/FlightInfo.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import HorizontalTextSection from '../components/HorizontalTextSection.js';
+import TextSection from '../components/TextSection.js';
 /* import flight info md here */
 
 class Section extends Component {
   render() {
     return (
       <div className='flight-info'>
-        <HorizontalTextSection>
+        <TextSection>
           <div className='flight-banner'>
             <h1>Flight Info</h1>
           </div>
           <div className='flight-info-info'>
-
           </div>
-        </HorizontalTextSection>
+        </TextSection>
       </div>
     );
   }
--- src/pages/History.js
+++ src/pages/History.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import HorizontalTextSection from '../components/HorizontalTextSection.js';
+import TextSection from '../components/TextSection.js';
 /* import history md here */
 
 class Section extends Component {
   render() {
     return (
       <div className='history-of-zanzibar'>
-        <HorizontalTextSection>
+        <TextSection>
           <div className='history-banner'>
             <h1>History of Zanzibar</h1>
           </div>
           <div className='history-info'>
-
           </div>
-        </HorizontalTextSection>
+        </TextSection>
       </div>
     );
   }
