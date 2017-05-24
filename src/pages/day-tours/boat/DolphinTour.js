04a6551d25295abe896e806398d2772335b299ad
Igor Banaszuk <igorbanaszuk@Igors-MacBook-Pro.local>
2017-05-24 15:52:34 -0600

    all the day tour pages are set up EXCEPT chumbe island, city tour, and spice tour

--- src/pages/day-tours/boat/DolphinTour.js
+++ src/pages/day-tours/boat/DolphinTour.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import dolphinMark from '../../../content/DolphinTours.md';
+import HorizontalTextSection from '../../../components/HorizontalTextSection.js';
 
 class DolpinTour extends Component {
   render() {
     return (
-      <div className="tour">
-        Tour
+      <div className="dolphin-page">
+          <div className="dolphin-banner">
+            <h1>Dolphin Tour</h1>
+          </div>
+          <div className="dolphin-info">
+           <HorizontalTextSection>
+            <div className='dolphin-markdown' dangerouslySetInnerHTML={{__html: dolphinMark}}>
+            </div>
+            </HorizontalTextSection>
+          </div>
       </div>
     );
   }
--- src/pages/day-tours/boat/PrisonIsland.js
+++ src/pages/day-tours/boat/PrisonIsland.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import prisonMark from '../../../content/PrisonIsland.md';
+import HorizontalTextSection from '../../../components/HorizontalTextSection.js';
 
 class PrisonIsland extends Component {
   render() {
     return (
-      <div className="tour">
-        Tour
+      <div className="prison-page">
+          <div className="prison-banner">
+            <h1>Prison Island</h1>
+          </div>
+          <div className="prison-info">
+           <HorizontalTextSection>
+            <div className='prison-markdown' dangerouslySetInnerHTML={{__html: prisonMark}}>
+            </div>
+            </HorizontalTextSection>
+          </div>
       </div>
     );
   }
