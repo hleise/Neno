be50938c963464ed3069648615a7e97c823e7024
Igor Banaszuk <igba2150@colorado.edu>
2017-05-26 14:08:06 -0600

    added safaris' .scss files

---
+++ src/pages/safaris/northern/Arusha.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import HorizontalTextSection from '../../../components/HorizontalTextSection.js';
+
+
+class Arusha extends Component {
+  render() {
+    return (
+      <div className="arusha-page">
+          <div className="arusha-banner">
+            <h1>Arusha National Park</h1>
+          </div>
+          <div className="arusha-info">
+           <HorizontalTextSection>
+
+            </HorizontalTextSection>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default Arusha;
