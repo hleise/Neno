4bc500a2ca23bbae597ffc4f6df74a932232d6e6
Igor Banaszuk <igba2150@colorado.edu>
2017-06-01 14:12:49 -0600

    logo tab is to the far left and the other tabs are to the right now! The ABOUT tab has history of zanzibar and volunteers as dropdown optionsand I also added a gallery tab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,15 @@ class NavBar extends Component {
       <div>
         <div className='nav-bar-container'>
           <div className='nav-bar'>
+          <div className='nav-bar-button'>
+            <div className='nav-bar-button-title'>
+              <h3>
+                <Link to="">
+                   <img className='logo' src={require('../logos/logo.png')} />
+                </Link>
+              </h3>
+            </div>
+          </div>
             <div className='nav-bar-button'>
               <div className='nav-bar-button-title'>
                 <h3>
@@ -76,26 +85,33 @@ class NavBar extends Component {
             <div className='nav-bar-button'>
               <div className='nav-bar-button-title'>
                 <h3>
-                  <Link to="/history-of-zanzibar">
-                    History of Zanzibar
-                  </Link>
-                </h3>
-              </div>
-            </div>
-            <div className='nav-bar-button'>
-              <div className='nav-bar-button-title'>
-                <h3>
-                  <Link to="/volunteers">
-                     Volunteers
-                  </Link>
-                </h3>
-              </div>
-            </div>
-            <div className='nav-bar-button'>
-              <div className='nav-bar-button-title'>
-                <h3>
                   <Link to="/about">
-                     <img className='logo' src={require('../logos/logo.png')} />
+                     About
+                  </Link>
+                </h3>
+              </div>
+              <div className='dropdown-options'>
+                <div className='dropdown-option'>
+                  <h3>
+                    <Link to="/volunteers">
+                      Volunteers
+                    </Link>
+                  </h3>
+                </div>
+                <div className='dropdown-option'>
+                  <h3>
+                    <Link to="/history-of-zanzibar">
+                      History of Zanzibar
+                    </Link>
+                  </h3>
+                </div>
+            </div>
+          </div>
+            <div className='nav-bar-button'>
+              <div className='nav-bar-button-title'>
+                <h3>
+                  <Link to="/gallery">
+                     Gallery
                   </Link>
                 </h3>
               </div>
@@ -107,5 +123,5 @@ class NavBar extends Component {
     );
   }
 }
- /*<img className='logo' src={require('../logos/logo.png')} />*/
+
 export default NavBar;
