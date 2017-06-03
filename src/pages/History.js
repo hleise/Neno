4106a66027d66bbf5d761aca3ed14c5e15026de8
jose.canizares <jose.canizares@workday.com>
2017-06-02 18:29:58 -0600

    Edit history page

--- src/pages/History.js
+++ src/pages/History.js
@@ -2,21 +2,25 @@ import React, { Component } from 'react';
 import StaticBanner from '../layout/StaticBanner';
 import HorizontalTextSection from '../components/HorizontalTextSection.js';
 import historyMark from '../content/HistoryOfZanzibar.md';
+import PageLayout from '../layout/PageLayout';
 
 class History extends Component {
   render() {
     return (
-      <div className='history'>
-      <StaticBanner
-        bannerTitle='History of Zanzibar'
-        page='history'
-        i='0'
-        />
-        <HorizontalTextSection>
-         <div className='history-markdown' dangerouslySetInnerHTML={{__html: historyMark}}>
-         </div>
-         </HorizontalTextSection>
-         </div>
+      <PageLayout>
+      <div className="page">
+          <div className="banner">
+            <h1>History Of Zanzibar</h1>
+            <img className='banner-image' src={require('../img/history/0.jpg')} />
+          </div>
+          <div className="info">
+           <HorizontalTextSection>
+            <div className='markdown' dangerouslySetInnerHTML={{__html: historyMark}}>
+            </div>
+            </HorizontalTextSection>
+          </div>
+      </div>
+      </PageLayout>
     );
   }
 }
