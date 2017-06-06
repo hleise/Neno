458d85c3f766453777f3cb1aa51cc78b948bf525
Igor Banaszuk <igba2150@colorado.edu>
2017-06-06 15:02:50 -0600

    txt is still not centered in the tiles of the gallery :(

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Gallery extends Component {
   render() {
     return (
+      <page>
       <div className='image-wrapper'>
         <div className='image'>
           <img className='left-image' src={require('../img/5.jpg')} alt='left'/>
@@ -13,6 +14,17 @@ class Gallery extends Component {
           <h1>Adventure</h1>
         </div>
       </div>
+      <div className='image-wrapper'>
+        <div className='image'>
+          <img className='left-image' src={require('../img/5.jpg')} alt='left'/>
+          <h1>Featured</h1>
+        </div>
+        <div className='image'>
+          <img className='right-image' src={require('../img/4.jpg')} alt='right'/>
+          <h1>Adventure</h1>
+        </div>
+      </div>
+    </page>
     );
   }
 }
