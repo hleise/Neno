dfea9e56e6049c7d417a11193f28e8623f873ff8
Igor Banaszuk <igba2150@colorado.edu>
2017-06-08 16:16:01 -0600

    image -> gallery-image-container

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,37 +7,37 @@ class Gallery extends Component {
 
       <page>
       <div className='image-row'>
-        <div className='image'>
+        <div className='gallery-image-container'>
           <h1>Featured</h1>
           <img className='gallery-image' src={require('../img/gallery/0.jpg')} alt='left'/>
         </div>
-        <div className='image'>
+        <div className='gallery-image-container'>
           <h1>Adventure</h1>
           <img className='gallery-image' src={require('../img/gallery/1.jpg')} alt='right'/>
         </div>
       </div>
       <div className='image-row'>
-        <div className='image'>
+        <div className='gallery-image-container'>
           <h1>Mainland Tanzania</h1>
           <img className='gallery-image' src={require('../img/gallery/2.jpg')} alt='left'/>
         </div>
-        <div className='image'>
+        <div className='gallery-image-container'>
           <h1>Zanzibar Island</h1>
           <img className='gallery-image' src={require('../img/gallery/3.jpg')} alt='right'/>
         </div>
       </div>
       <div className='image-row'>
-        <div className='image'>
+        <div className='gallery-image-container'>
           <h1>Culture</h1>
           <img className='gallery-image' src={require('../img/gallery/4.jpg')} alt='left'/>
         </div>
-        <div className='image'>
+        <div className='gallery-image-container'>
           <h1>Volunteer Opportunities</h1>
           <img className='gallery-image' src={require('../img/gallery/5.jpg')} alt='right'/>
         </div>
       </div>
     </page>
-  
+
     );
   }
 }
