a630148909f9268699f7670cf2ec059ff07046fd
jose.canizares <jose.canizares@workday.com>
2017-06-11 14:35:22 -0600

    h1 now disappears when menu is open

--- src/layout/Stack.js
+++ src/layout/Stack.js
@@ -33,18 +33,18 @@ class Stack extends Component {
       <div className={this.props.className}>
         {this.props.data.map((section, i) =>
         <div className='section' id={section.sectionTitle.replace(/\s+/g, '-').toLowerCase()}>
-          <Banner
-            page={this.props.page}
-            sectionTitle={section.sectionTitle}
-            toggleMenu={this.toggleMenu.bind(this, i)}
-            i={i}
-            />
           <Menu
             menu={this.state.menus[i]}
             featuredTours={section.featuredTours}
             page={this.props.page}
             imgFolder={section.imgFolder}
             toggleMenu={this.toggleMenu.bind(this, i)}
+            />
+          <Banner
+            page={this.props.page}
+            sectionTitle={section.sectionTitle}
+            toggleMenu={this.toggleMenu.bind(this, i)}
+            i={i}
             />
         </div>
         )}
