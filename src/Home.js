8833b1a27bdbc69d381af1bc9cdeaf274d722214
Igor Banaszuk <igba2150@colorado.edu>
2017-06-11 17:11:24 -0600

    carousel slider works

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,8 @@ import HorizontalTextSection from './components/HorizontalTextSection.js';
 import HomeContent from './content/home.md';
 import PageLayout from './layout/PageLayout';
 import Slider from './components/Slider';
+var Carousel = require('nuka-carousel');
+
 
 class Home extends Component {
   rawMarkup(){
@@ -11,8 +13,17 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <Slider/>
         <PageLayout>
+          <Carousel>
+            <img src={require('./img/home/0.jpg')}/>
+            <img src={require('./img/home/1.jpg')}/>
+            <img src={require('./img/home/2.jpg')}/>
+            <img src={require('./img/home/3.jpg')}/>
+            <img src={require('./img/home/4.jpg')}/>
+            <img src={require('./img/home/5.jpg')}/>
+            <img src={require('./img/home/6.jpg')}/>
+            <img src={require('./img/home/7.jpg')}/>
+          </Carousel>
           <div className="page">
               <div className="info">
                 <HorizontalTextSection>
