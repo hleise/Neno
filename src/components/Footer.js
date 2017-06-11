0097fbc6c89cb4219d2e257e68b901bfd13821ab
jose.canizares <jose.canizares@workday.com>
2017-06-11 15:08:06 -0600

    add styling for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,15 @@ class Footer extends Component {
   render() {
     return (
       <div className='footer'>
-        <h3>
-          © Neno Tours & Travels
-        </h3>
+        <h2>© Neno Tours & Travels</h2>
         <a href="https://www.facebook.com/NenoToursNtravels" rel="external" target="_blank">
-           <img className='fb-logo' src={require('../logos/facebook.png')} />
+           <img className='logo' src={require('../logos/facebook.png')} />
         </a>
         <a href="https://www.tripadvisor.com/Attraction_Review-g488129-d7391505-Reviews-Neno_Tours_Travel_Agency_Day_Tours-Stone_Town_Zanzibar_City_Zanzibar_Island_Zanzi.html" rel="external" target="_blank">
-          <img className='ta-logo' src={require('../logos/tripadvisor.png')} />
+          <img className='logo' src={require('../logos/tripadvisor.png')} />
         </a>
         <Link to="contact">
-          Contact
+          <h2>Contact</h2>
         </Link>
       </div>
     );
