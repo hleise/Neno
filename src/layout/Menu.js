e7ce6ab0fb5e1a5fac20387ee9b6cafdf94ad5d0
jose.canizares <jose.canizares@workday.com>
2017-06-22 23:54:08 -0600

    dynamic column calculation

--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Menu extends Component {
+  getWidth() {
+    return 1
+  }
   render() {
+    var col = Math.floor(document.documentElement.clientWidth / 430);
     return (
       <div className={"menu " + this.props.menu} onClick={this.props.toggleMenu}>
        <div className='featured-tours'>
-          {[...Array(Math.ceil(this.props.featuredTours.length / 4))].map((rowOfTours, rowIndex) => (
+          {[...Array(Math.ceil(this.props.featuredTours.length / col))].map((rowOfTours, rowIndex) => (
               <div className='row' key={rowIndex}>
-                {this.props.featuredTours.slice(rowIndex*4,(rowIndex*4)+4).map((tour, i) =>
+                {this.props.featuredTours.slice(rowIndex*col,(rowIndex*col)+col).map((tour, i) =>
                   <div className='block' key={i}>
-                    <img className='image' src={require("../img/" + this.props.page + "/"  + this.props.imgFolder + "/" + (i+(rowIndex*4)) + ".jpg")} />
+                    <img className='image' src={require("../img/" + this.props.page + "/"  + this.props.imgFolder + "/" + (i+(rowIndex*col)) + ".jpg")} />
                      <Link to={"/" + this.props.page + "/" + tour.replace(/\s+/g, '-').toLowerCase()}>
                       <div className='title'>
                         <h3>
