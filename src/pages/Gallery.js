0ae8b308c51face1eeac6c3682a218738c49916e
jose.canizares <jose.canizares@workday.com>
2017-06-22 22:28:36 -0600

    gallery category background is now a link itself

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -6,61 +6,50 @@ import {Link} from 'react-router-dom';
 class Gallery extends Component {
   render() {
     return (
-
       <page>
       <div className='image-row'>
-        <div className='gallery-image-container'>
+        <Link to="/gallery-category/featured" className='gallery-image-container'>
           <h1>
             <Link to="/gallery-category/featured">
                 Featured
             </Link>
           </h1>
           <img className='gallery-image' src={require('../img/gallery/0.jpg')} alt='left'/>
-        </div>
-        <div className='gallery-image-container'>
-          <h1>
-            <Link to="/gallery-category/adventure">
-              Adventure
-            </Link>
-          </h1>
-          <img className='gallery-image' src={require('../img/gallery/1.jpg')} alt='right'/>
-        </div>
+        </Link>
+        <Link to="/gallery-category/adventure" className='gallery-image-container'>
+              <h1>
+                Adventure
+              </h1>
+            <img className='gallery-image' src={require('../img/gallery/1.jpg')} alt='right'/>
+        </Link>
       </div>
       <div className='image-row'>
-        <div className='gallery-image-container'>
+        <Link to="/gallery-category/mainland-tanzania" className='gallery-image-container'>
           <h1>
-            <Link to="/gallery-category/mainland-tanzania">
               Mainland Tanzania
-            </Link>
           </h1>
           <img className='gallery-image' src={require('../img/gallery/2.jpg')} alt='left'/>
-        </div>
-        <div className='gallery-image-container'>
+        </Link>
+        <Link to="/gallery-category/zanzibar-island" className='gallery-image-container'>
           <h1>
-            <Link to="/gallery-category/zanzibar-island">
               Zanzibar Island
-            </Link>
           </h1>
           <img className='gallery-image' src={require('../img/gallery/3.jpg')} alt='right'/>
-        </div>
+        </Link>
       </div>
       <div className='image-row'>
-        <div className='gallery-image-container'>
+        <Link to="/gallery-category/culture" className='gallery-image-container'>
           <h1>
-            <Link to="/gallery-category/culture">
               Culture
-            </Link>
           </h1>
           <img className='gallery-image' src={require('../img/gallery/4.jpg')} alt='left'/>
-        </div>
-        <div className='gallery-image-container'>
+        </Link>
+        <Link to="/gallery-category/volunteer-opportunities" className='gallery-image-container'>
           <h1>
-            <Link to="/gallery-category/volunteer-opportunities">
               Volunteer Opportunities
-            </Link>
           </h1>
           <img className='gallery-image' src={require('../img/gallery/5.jpg')} alt='right'/>
-        </div>
+        </Link>
       </div>
     </page>
 
