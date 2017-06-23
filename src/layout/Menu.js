e90e3593e7a4db296b0a9ee271d2a88fc17a5c26
Igor Banaszuk <igba2150@colorado.edu>
2017-06-23 00:17:10 -0600

    dynamic column count

--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -6,7 +6,7 @@ class Menu extends Component {
     return 1
   }
   render() {
-    var col = Math.floor(document.documentElement.clientWidth / 430);
+    var col = Math.floor(document.documentElement.clientWidth / 300);
     return (
       <div className={"menu " + this.props.menu} onClick={this.props.toggleMenu}>
        <div className='featured-tours'>
@@ -18,7 +18,7 @@ class Menu extends Component {
                      <Link to={"/" + this.props.page + "/" + tour.replace(/\s+/g, '-').toLowerCase()}>
                       <div className='title'>
                         <h3>
-                         {this.props.featuredTours[(i+(rowIndex*4))]}
+                         {this.props.featuredTours[(i+(rowIndex*col))]}
                         </h3>
                       </div>
                     </Link>
