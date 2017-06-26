ed735563ff1f944df90339c914d97ca2a59d974a
Igor Banaszuk <igba2150@colorado.edu>
2017-06-26 17:06:13 -0600

    flight info page now has a markdown

--- src/pages/FlightInfo.js
+++ src/pages/FlightInfo.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import StaticBanner from '../layout/StaticBanner';
 import PageLayout from '../layout/PageLayout';
 import HorizontalTextSection from '../components/HorizontalTextSection.js';
+import flightMark from '../content/FlightInfo.md';
 
 class FlightInfo extends Component {
   render() {
     return (
       <PageLayout>
+      <div className="page">
         <StaticBanner
           bannerTitle='Flight Info'
           page='flight-info'
@@ -14,9 +16,11 @@ class FlightInfo extends Component {
           />
         <div className="info">
          <HorizontalTextSection>
-
+          <div className='markdown' dangerouslySetInnerHTML={{__html: flightMark}}>
+          </div>
           </HorizontalTextSection>
         </div>
+      </div>
       </PageLayout>
     );
   }
