7129c327b1dd196fb5f2b090ecefd4e62a5f15b6
Hunter Leise <hleise27@gmail.com>
2017-08-04 15:04:48 -0600

    Add top padding to page-layout, so the fixed menu doesn't cover content.

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,61 +1,62 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-
+import PageLayout from '../layout/PageLayout';
 
 class Gallery extends Component {
   render() {
     return (
-      <page className='gallery'>
-      <div className='image-row'>
-        <Link to="/gallery-category/featured" className='gallery-image-container'>
-          <h1>
-            Featured |
-            Mandhari
-          </h1>
-          <img className='gallery-image' src={require('../img/gallery/0.jpg')} alt='left'/>
-        </Link>
-        <Link to="/gallery-category/adventure" className='gallery-image-container'>
+      <PageLayout>
+        <page className='gallery'>
+        <div className='image-row'>
+          <Link to="/gallery-category/featured" className='gallery-image-container'>
             <h1>
-              Adventure |
-              Mtiti
+              Featured |
+              Mandhari
             </h1>
-            <img className='gallery-image' src={require('../img/gallery/1.jpg')} alt='right'/>
-        </Link>
-      </div>
-      <div className='image-row'>
-        <Link to="/gallery-category/mainland-tanzania" className='gallery-image-container'>
-          <h1>
-            Mainland Tanzania |
-            Tanzania Bara
-          </h1>
-          <img className='gallery-image' src={require('../img/gallery/2.jpg')} alt='left'/>
-        </Link>
-        <Link to="/gallery-category/zanzibar-island" className='gallery-image-container'>
-          <h1>
-            Zanzibar Island |
-            Kisiwa Cha Zanzibar
-          </h1>
-          <img className='gallery-image' src={require('../img/gallery/3.jpg')} alt='right'/>
-        </Link>
-      </div>
-      <div className='image-row'>
-        <Link to="/gallery-category/culture" className='gallery-image-container'>
-          <h1>
-            Culture |
-            Utamaduni
-          </h1>
-          <img className='gallery-image' src={require('../img/gallery/4.jpg')} alt='left'/>
-        </Link>
-        <Link to="/gallery-category/volunteer-opportunities" className='gallery-image-container'>
-          <h1>
-            Volunteer Opportunities |
-            Fursa Za Kujitolea
-          </h1>
-          <img className='gallery-image' src={require('../img/gallery/5.jpg')} alt='right'/>
-        </Link>
-      </div>
-    </page>
-
+            <img className='gallery-image' src={require('../img/gallery/0.jpg')} alt='left'/>
+          </Link>
+          <Link to="/gallery-category/adventure" className='gallery-image-container'>
+              <h1>
+                Adventure |
+                Mtiti
+              </h1>
+              <img className='gallery-image' src={require('../img/gallery/1.jpg')} alt='right'/>
+          </Link>
+        </div>
+        <div className='image-row'>
+          <Link to="/gallery-category/mainland-tanzania" className='gallery-image-container'>
+            <h1>
+              Mainland Tanzania |
+              Tanzania Bara
+            </h1>
+            <img className='gallery-image' src={require('../img/gallery/2.jpg')} alt='left'/>
+          </Link>
+          <Link to="/gallery-category/zanzibar-island" className='gallery-image-container'>
+            <h1>
+              Zanzibar Island |
+              Kisiwa Cha Zanzibar
+            </h1>
+            <img className='gallery-image' src={require('../img/gallery/3.jpg')} alt='right'/>
+          </Link>
+        </div>
+        <div className='image-row'>
+          <Link to="/gallery-category/culture" className='gallery-image-container'>
+            <h1>
+              Culture |
+              Utamaduni
+            </h1>
+            <img className='gallery-image' src={require('../img/gallery/4.jpg')} alt='left'/>
+          </Link>
+          <Link to="/gallery-category/volunteer-opportunities" className='gallery-image-container'>
+            <h1>
+              Volunteer Opportunities |
+              Fursa Za Kujitolea
+            </h1>
+            <img className='gallery-image' src={require('../img/gallery/5.jpg')} alt='right'/>
+          </Link>
+        </div>
+      </page>
+    </PageLayout>
     );
   }
 }
