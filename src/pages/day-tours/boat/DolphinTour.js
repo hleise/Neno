30a50ca7a60b872d24f0ff87ef3a762208b62dc6
José Cañizares <jose.canizares@colorado.edu>
2017-08-15 16:00:01 -0600

    modify a few pages to use page component

--- src/pages/day-tours/boat/DolphinTour.js
+++ src/pages/day-tours/boat/DolphinTour.js
@@ -1,28 +1,18 @@
 import React, {Component} from 'react';
-import dolphinMark from '../../../content/day-tours/boat/DolphinTours.md';
-import HorizontalTextSection from '../../../components/HorizontalTextSection.js';
-import BookingButton from "../../../components/BookingButton";
-import Banner from '../../../layout/Banner'
+import markdown from '../../../content/day-tours/boat/DolphinTours.md';
+import Page from     '../../../layout/Page'
 
-class DolpinTour extends Component {
+class DolphinTour extends Component {
   render() {
     return (
-      <div className="page">
-        <Banner
-          title='Dolphin Tour'
-          page='day-tours/boat'
-          i='3'
-        />
-        <div className="info" id="info">
-          <HorizontalTextSection>
-            <div className='markdown' dangerouslySetInnerHTML={{__html: dolphinMark}}>
-            </div>
-            <BookingButton/>
-          </HorizontalTextSection>
-        </div>
-      </div>
+      <Page
+        markdown={markdown}
+        title='Dolphin Tour'
+        page={'day-tours/boat'}
+        i={3}
+      />
     );
   }
 }
 
-export default DolpinTour;
+export default DolphinTour;
--- src/pages/day-tours/boat/PrisonIsland.js
+++ src/pages/day-tours/boat/PrisonIsland.js
@@ -1,26 +1,16 @@
 import React, {Component} from 'react';
-import prisonMark from '../../../content/day-tours/boat/PrisonIsland.md';
-import HorizontalTextSection from '../../../components/HorizontalTextSection.js';
-import BookingButton from "../../../components/BookingButton";
-import Banner from '../../../layout/Banner'
+import markdown from '../../../content/day-tours/boat/PrisonIsland.md';
+import Page from     '../../../layout/Page'
 
 class PrisonIsland extends Component {
   render() {
     return (
-      <div className="page">
-        <Banner
-          title='Prison Island'
-          page='day-tours/boat'
-          i='4'
-        />
-        <div className="info" id="info">
-          <HorizontalTextSection>
-            <div className='markdown' dangerouslySetInnerHTML={{__html: prisonMark}}>
-            </div>
-            <BookingButton/>
-          </HorizontalTextSection>
-        </div>
-      </div>
+      <Page
+        markdown={markdown}
+        title='Prison Island'
+        page={'day-tours/boat'}
+        i={4}
+      />
     );
   }
 }
